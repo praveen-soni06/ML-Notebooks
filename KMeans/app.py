8617bb4f15a18f3302cf925f45f591313d41edf0
praveen-soni06 <praveenkumar12032@gmail.com>
2026-05-12 23:29:03 +0530

    Knn implementation by own Class

--- KMeans/app.py
+++ KMeans/app.py
@@ -1,1 +1,11 @@
-from kmeans import KMeans
+# from kmeans import KMeans
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+centroid = [(-5,-5), (5,5)]
+cluster_std = [1,1]
+
+X,y = make_blobs(n_samples=100, cluster_std=cluster_std, centers=centroid, n_features=2, random_state=2)
+
+plt.scatter(X[:,0], X[:,1])
+plt.show()
